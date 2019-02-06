3aa73d104d908c3251fa49e2c363a092f9a6fdcb
PC-20160804PEHZ\Administrator <944284742@qq.com>
2019-02-07 00:17:02 +0800

    v1 //修改过的

--- APP_Addons_0001_Message/app/src/main/java/com/example/administrator/app_addons_0001_message/MainActivity.java
+++ APP_Addons_0001_Message/app/src/main/java/com/example/administrator/app_addons_0001_message/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.administrator.app_addons_0001_message;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Printer;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import	android.view.View;
-import	android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private  Button mButton;

da365a1ce7cd9a02ba5223d10fe96bae6d3694c2
PC-20160804PEHZ\Administrator <944284742@qq.com>
2019-02-06 21:24:00 +0800

    v2：//create thread

--- APP_Addons_0001_Message/app/src/main/java/com/example/administrator/app_addons_0001_message/MainActivity.java
+++ APP_Addons_0001_Message/app/src/main/java/com/example/administrator/app_addons_0001_message/MainActivity.java
@@ -6,23 +6,65 @@ import android.util.Printer;
 import android.widget.Button;
 import	android.view.View;
 import	android.util.Log;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
-    private  Button mButton;
     private final String TAG = "MessageTest";
     private  int ButtonCount = 0;
+    private Thread mythread = null;
+    private MyThread mythread2 = null;
+
+    class MyRunndble implements Runnable {
+
+        @Override
+        public void run() {
+            int count = 0;
+            for (;;){
+                Log.d(TAG,"MyThread1 " + count);
+                count ++;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+    class MyThread extends Thread
+    {
+        public void run()
+        {
+            int count = 0;
+            for (;;){
+                Log.d(TAG,"MyThread2 " + count);
+                count ++;
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            mButton = findViewById(R.id.button);
-            mButton .setOnClickListener(new View.OnClickListener() {
+        Button mButton = findViewById(R.id.button);
+            mButton.setOnClickListener(new View.OnClickListener() {
                  public void onClick(View v) {
-                     Log.d(TAG,"Send Message"+"");
+                     Log.d(TAG,"Send Message"+ButtonCount);
                      ButtonCount++;
             }
         });
+        mythread = new Thread(new MyRunndble(), "MessageTestThread");
+        mythread.start();
 
+        mythread2 = new MyThread();
+        mythread2.start();
     }
 }

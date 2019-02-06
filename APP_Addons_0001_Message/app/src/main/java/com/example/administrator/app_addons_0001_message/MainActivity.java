e685897aa48bc1335c3e8afeb80ba87149d2a003
PC-20160804PEHZ\Administrator <944284742@qq.com>
2019-02-06 23:05:18 +0800

    v3 : support Message

--- APP_Addons_0001_Message/app/src/main/java/com/example/administrator/app_addons_0001_message/MainActivity.java
+++ APP_Addons_0001_Message/app/src/main/java/com/example/administrator/app_addons_0001_message/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.administrator.app_addons_0001_message;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Printer;
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private  int ButtonCount = 0;
     private Thread mythread = null;
     private MyThread mythread2 = null;
+    private Handler mHandler;
+    private  int mMessageCount = 0;
 
     class MyRunndble implements Runnable {
 
@@ -33,19 +38,33 @@ public class MainActivity extends AppCompatActivity {
     }
     class MyThread extends Thread
     {
+        private Looper mLooper;
         public void run()
         {
-            int count = 0;
-            for (;;){
-                Log.d(TAG,"MyThread2 " + count);
-                count ++;
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
+            super.run();
+            Looper.prepare();
+            synchronized (this) {
+                mLooper = Looper.myLooper();
+                notifyAll();
             }
+            mLooper =  Looper.myLooper();
+            Looper.loop();
+        }
+        public  Looper getLooper(){
+            if (!isAlive()) {
+                return null;
+            }
+
+            // If the thread has been started, wait until the looper has been created.
+            synchronized (this) {
+                while (isAlive() && mLooper == null) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+            return mLooper;
         }
     }
 
@@ -54,11 +73,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mButton = findViewById(R.id.button);
-            mButton.setOnClickListener(new View.OnClickListener() {
+        Button mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
                  public void onClick(View v) {
                      Log.d(TAG,"Send Message"+ButtonCount);
                      ButtonCount++;
+                     Message msg = new Message();
+                     mHandler.sendMessage(msg);
             }
         });
         mythread = new Thread(new MyRunndble(), "MessageTestThread");
@@ -66,5 +87,14 @@ public class MainActivity extends AppCompatActivity {
 
         mythread2 = new MyThread();
         mythread2.start();
+
+        mHandler = new Handler(mythread2.getLooper(), new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                Log.d(TAG,"get Messsage"+mMessageCount);
+                mMessageCount++;
+                return false;
+            }
+        });
     }
 }
